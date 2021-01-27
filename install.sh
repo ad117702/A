d4632dbf6d6a28abc1e9a15086d956eff642a8f8
ad117702 <77816745+ad117702@users.noreply.github.com>
2021-01-27 12:17:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,14 @@ fi
 
 cp init_sql.sql /tmp/init_sql.sql -f
 
+# install influxdata
 wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 source /etc/lsb-release
 echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 
+sudo apt-get update && sudo apt-get install influxdb
+sudo systemctl unmask influxdb.service
+sudo systemctl start influxdb
 
 # install Grafana
 sudo apt-get install -y adduser libfontconfig1
